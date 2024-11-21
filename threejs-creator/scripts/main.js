7c23c790d7e59bf5980d43aa5f4b2c026f76834f
ELHANTpl <kontakt@elhant.pl>
2024-11-21 15:10:40 +0100

    dodaj

--- threejs-creator/scripts/main.js
+++ threejs-creator/scripts/main.js
@@ -1,23 +1,6 @@
 import { initScene, loadModel } from './loader.js';
 
 document.addEventListener("DOMContentLoaded", () => {
-  const container = document.getElementById('scena-edytora');
-
-  if (!container) {
-    console.error('Nie znaleziono elementu o ID "scena-edytora".');
-    return;
-  }
-
-  // Inicjalizacja sceny
-  const { scene, camera, renderer } = initScene(container);
-
-  // Wczytaj samochód
-  loadModel(scene, 'https://cdn.jsdelivr.net/gh/ELHANTpl/kreator3d/threejs-creator/src/models/car1/lambo.glb');
-});
-
-import { initScene, loadModel } from './loader.js';
-
-document.addEventListener("DOMContentLoaded", () => {
   console.log('DOMContentLoaded event triggered'); // Sprawdzenie, czy DOM jest załadowany
 
   const container = document.getElementById('scena-edytora');
